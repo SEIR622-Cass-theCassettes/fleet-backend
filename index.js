711a12e21c206bc6981be524074319cfd237abde
Joshua Favre <joshuaf@MacBook-Pro.local>
2020-08-14 17:43:10 -0500

    install cors and add to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
 
 app.set('port', process.env.PORT || 8000);
 
