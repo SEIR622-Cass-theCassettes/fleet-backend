18d1a5d5161fbb0d49e2827ceec898cf137185b3
Joshua Favre <joshuaf@MacBook-Pro.local>
2020-08-17 12:14:05 -0500

    npm install dotenv add mongoose error handlers to connection.js

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const trucksController = require('./controllers/trucks');
 const usersController = require('./controllers/users');
-
 const millageController = require('./controllers/millage');
-
 
 const app = express();
 
